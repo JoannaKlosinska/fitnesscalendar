32b1d834e64ed4dbd0e16ec2d2961b4f7d69427f
Joanna Klosinska <joanna.klosinska.55@gmail.com>
2020-12-19 16:11:17 +0100

    Add default schedules for coaches

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,7 +46,57 @@ end
     password_confirmation: password
   )
 end
-
+# Random schedules for coaches
+schedule1 = Coach.first.schedule
+schedule1.update(
+  monday: ["7", "8", "9", "10", "11", "16", "17", "18", "19", "20"],
+  tuesday: ["9", "10", "11", "12", "13"],
+  wednesday: ["7", "8", "9", "15", "16", "17", "18", "19"],
+  thursday: ["17", "18", "19", "20", "21"],
+  friday: ["7", "8", "9", "10", "11", "16", "17", "18", "19", "20"],
+  saturday: ["10", "11", "12"],
+  sunday: ["16", "17", "18"]
+)
+schedule2 = Coach.second.schedule
+schedule2.update(
+  monday: ["17", "18", "19", "20", "21"],
+  tuesday: ["7", "8", "9", "10", "14", "15"],
+  wednesday: ["15", "16", "17", "18", "19", "20"],
+  thursday: ["7", "8", "9", "10", "11", "12"],
+  friday: ["15", "16", "17"],
+  saturday: ["8", "9", "10", "11", "12"],
+  sunday: ["11", "12", "13"]
+)
+schedule3 = Coach.third.schedule
+schedule3.update(
+  monday: ["7", "8", "9", "10"],
+  tuesday: ["13", "14", "15", "16", "17"],
+  wednesday: ["7", "8", "9", "10", "11", "12"],
+  thursday: ["7", "8", "9", "10", "11", "12"],
+  friday: ["17", "18", "19", "20", "21"],
+  saturday: ["9", "10", "11"],
+  sunday: ["9", "10", "11"]
+)
+schedule4 = Coach.fourth.schedule
+schedule4.update(
+  monday: ["17", "18", "19", "20", "21"],
+  tuesday: ["17", "18", "19", "20", "21"],
+  wednesday: ["10", "11", "12", "13", "14", "15"],
+  thursday: ["10", "11", "12", "13", "14", "15"],
+  friday: ["12", "13", "14"],
+  saturday: ["12", "13", "14"],
+  sunday: ["12", "13", "14"]
+)
+schedule5 = Coach.fifth.schedule
+schedule5.update(
+  monday: ["9", "10", "11", "12", "13", "14", "20", "21"],
+  tuesday: ["20", "21"],
+  wednesday: ["7", "8", "19", "20", "21"],
+  thursday: ["7", "8", "9", "10", "11", "12"],
+  friday: ["18", "19", "20", "21"],
+  saturday: ["8", "9", "10", "11", "12"],
+  sunday: ["12", "13"]
+)
 # Bookings
 20.times do |b|
   day = Time.zone.today + (1..20).to_a.sample.days
