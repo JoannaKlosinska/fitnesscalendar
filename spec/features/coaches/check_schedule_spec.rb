efc6b8a5af873e0c04feedb5a1df410a1433935c
Joanna Klosinska <joanna.klosinska.55@gmail.com>
2021-01-15 15:22:20 +0100

    Write test with capybara for coaches schedule

--- spec/features/coaches/check_schedule_spec.rb
+++ spec/features/coaches/check_schedule_spec.rb
@@ -1,5 +1,18 @@
-require "rails_helper"
+require 'rails_helper'
 
-feature "Schedule" do
-  
+feature 'Schedule' do
+  fixtures(:coaches)
+
+  scenario 'A coach update a schedule successfully' do
+    Schedule.create(coach: Coach.first)
+
+    sign_in_coach('foo@baz.com')
+    click_link 'Schedule'
+    within(find('tr', text: '10:00 - 11:00')) do
+      first("input[type='checkbox']").set(true)
+    end
+    click_on 'Update'
+    expect(page).to have_content 'Updated!'
+    expect(current_path). to eq(schedule_path)
+  end
 end
--- spec/support/features/sign_in.rb
+++ spec/support/features/sign_in.rb
@@ -6,4 +6,11 @@ module Features
     fill_in 'Password', with: 'password' 
     click_button 'Sign in'
   end
+
+  def sign_in_coach(email)
+    visit '/coach/sign_in'
+    fill_in 'Email', with: email
+    fill_in 'Password', with: 'password'
+    click_button 'Sign in'
+  end
 end
